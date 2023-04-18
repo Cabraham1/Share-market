021c1b7793b29d13c063df6570827251f769400c
cabraham1 <christopherabraham8@gmail.com>
2023-04-18 14:31:29 +0100

    Setup initial redux state

--- src/redux/stock/stockSlice.js
+++ src/redux/stock/stockSlice.js
@@ -6,3 +6,12 @@ const FILTER_COMPANY = 'stocks-market-trends/stocks/FILTER_COMPANY';
 const RESET_STOCK = 'stocks-market-trends/stocks/RESET_STOCK';
 const API_URL = 'https://financialmodelingprep.com/api/v3/';
 const API_KEY = '82a4de9376f4c4d59d47b606435ee1e1';
+
+const initialState = {
+  stockData: [],
+  companyDetails: {},
+  companyStatements: {},
+  filteredCompany: {},
+  loading: false,
+  error: null,
+};
